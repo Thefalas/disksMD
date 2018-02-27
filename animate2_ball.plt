419506a7daeca7ddf8fe3fe21c2a0707e5e05ee1
malopez <malopez00@gmail.com>
2018-02-27 13:31:03 +0100

    27-02-18 changes
    Needs bugfixing

---
+++ animate2_ball.plt
@@ -0,0 +1,28 @@
+# ANIMATE BALLS for MD codes (disks)
+
+# usage example: "gnuplot -e 'nt=900; LX=10; LY=10' --persist animate2_ball.gnuplot"
+
+set t wxt
+#set t png
+
+nt=900
+LX=80
+LY=45
+
+set xrange[0:LX]
+set yrange[0:LY]
+set size ratio LY/LX
+
+cd 'data'
+
+do for[count=0:nt]{
+#    pause 0.5
+    frame = 'xy'.count
+    if (count<1000) {frame='xy0'.count}
+    if (count<100) {frame='xy00'.count}
+    if (count<10) {frame='xy000'.count}
+    archivo = frame.'.dat'
+#    image = frame.'.png'
+#    set o image
+    plot archivo u 1:2:(1) w circles title archivo
+}
--- src/hardDisksMD.py
+++ src/hardDisksMD.py
@@ -11,8 +11,8 @@ import numpy as np
 data_folder = "C:/Users/malopez/Desktop/disksMD/data"
 restitution_coef = 1.0
 particle_radius = 1.0
-n_particles = 300
-desired_collisions_per_particle = 8
+n_particles = 150
+desired_collisions_per_particle = 6
 n_collisions = n_particles*desired_collisions_per_particle
 
 scale_factor = 80
@@ -81,11 +81,15 @@ def initializeRandom():
     
 def distance(i, j):
     """ Returns the distance between two particles i, j as a numpy array """
+    i = int(i)
+    j = int(j)
     dist = pos[i] - pos[j]
     return dist
 
 def distanceModulus(i, j):
     """ Measures the distance modulus between two particles i, j """
+    i = int(i)
+    j = int(j)
     dist_X = pos_X[j] - pos_X[i]
     dist_Y = pos_Y[j] - pos_Y[i]
     dist = math.sqrt(dist_X*dist_X + dist_Y*dist_Y)
@@ -93,6 +97,8 @@ def distanceModulus(i, j):
 
 def relativeVelocity(i, j):
     """ Measures the relative velocity between two particles i, j """
+    i = int(i)
+    j = int(j)
     rel_v = vel[i] - vel[j]
     return rel_v
 
@@ -109,16 +115,15 @@ def nanIfNegative(t):
         return t
 
 def propagate(t):
-    global pos
+    global pos, vel
     """ Updates positions for all particles, lineal movement during a time t"""
     for i in range(n_particles):
-        pos[i] = pos[i] + vel[i]*t
-
-    #times_pp[:,2] = times_pp[:,2] - t
-    #times_pp[:,2] = times_pp[:,2] - t    
+        pos[i] = pos[i] + vel[i]*t  
 
 def detectCollisionTime(i, j):
     """ Returns the time until the next collision between particles i, j """
+    i = int(i)
+    j = int(j)
     r = distance(i, j)
     r2 = np.dot(r, r)
     v = relativeVelocity(i, j)
@@ -143,6 +148,8 @@ def detectCollisionTime(i, j):
     
 def detectWallCollisionTime(i):
     """ Returns the time until particle i hits a wall """
+    i = int(i)
+
     x = pos[i, 0]
     y = pos[i, 1]
     vx = vel[i, 0]
@@ -169,10 +176,14 @@ def detectWallCollisionTime(i):
     # one found at: https://stackoverflow.com/a/2828121
     # More complex cause float conversion is needed for times column in array.
     times_pw = times_pw[np.array([float(a) for a in times_pw[:,2]]).argsort()]
-    return times_pw[0]
+    # We need to reshape to return a 2D array, although with just one row
+    return times_pw[0].reshape(1,-1)
 
 def wallCollision(i, wall):
     global vel
+    i = int(i)
+    wall = str(wall)
+
     if (wall=='left' or wall=='right'):
         vel[i,0] = -restitution_coef * vel[i,0] # x component changes direction
         vel[i,1] = restitution_coef * vel[i,1]
@@ -182,21 +193,19 @@ def wallCollision(i, wall):
 
 def particleCollision(i, j):
     global vel
-    vel[i] = vel[i] #Operaciones necesarias, habria que añadir un parámetro masa
-    vel[j] = vel[j]
+    i = int(i)
+    j = int(j)
     
-def updateTimeLists(i, j='none'):
-    """ Deletes all registries involving particles i, j and recalculates
-        collision times involving those particles, j as an optional param. """
-    if j=='none':
-        # Recalcula solo las colisiones relacionadas con i
-        times_pw[i] = detectWallCollisionTime(i)
-        #times_pp =
-    else:
-        # Recalcula las colisiones relacionadas con i, j
-        times_pw[i] = detectWallCollisionTime(i)
-        times_pw[j] = detectWallCollisionTime(j)
-
+    r = distance(i, j)
+    v = relativeVelocity(i, j)
+    b = np.dot(r, v)
+    # The following formula has been taken from Eq: 14.2.4 in
+    # 'The Art of Molecular Dynamics Simulations', D. Rapaport.
+    delta_v = (-b/(4*particle_radius*particle_radius))*r
+    # TODO: a mass parameter would be useful whe expanding functionality
+    vel[i] = vel[i] + delta_v 
+    vel[j] = vel[j] - delta_v
+    
 def createWallCollisionList():
     """ Creates an ordered list times_pw with collision times 
         for all particle-wall events """
@@ -224,65 +233,100 @@ def updateCollisionLists(t, i, j='none'):
         has passed since last collision, deletes and recalculates all entries 
         involving particles that interacted in last collision """
     global times_pp, times_pw
+    i = int(i)
     # Advances all entries a time t (time since last collision)
     times_pp[:,2] = times_pp[:,2] - t
     times_pp[:,2] = times_pp[:,2] - t
     
     if j=='none':
-        # Particle-Wall collision, recalculates only entries involving i part.
+        # Particle-Wall collision, recalculates only entries involving part. i
         # Delete certain entries, solution inspired by:
         # https://www.w3resource.com/python-exercises/numpy/python-numpy-exercise-91.php
         times_pp = times_pp[~np.isin(times_pp, [i]).any(axis=1)]
         times_pw = times_pw[~np.isin(times_pw, [str(i)]).any(axis=1)]
         
-        #Needed due to formating issues with times_pw
+        # Need float conversion due to formating issues with times_pw
         times_pw_float = np.array([float(a) for a in times_pw[:,2]])
-        
+        # Now we calculate new elements and insert them in an ordered manner
         for a in range(n_particles):
-            if i==a: # Avoid i-i case
+            if a==i and a!=n_particles-1: # Avoid i-i case
                 a=a+1
             new_entry = detectCollisionTime(i, a)
-            index = bisect.bisect(times_pp[:,2], new_entry[0,2])
-            times_pp = np.insert(times_pp, index, new_entry)
+            if new_entry[0,2] == 'inf':
+                index = -1
+            else:
+                index = bisect.bisect(times_pp[:,2], float(new_entry[0,2]))
+            times_pp = np.insert(times_pp, index, new_entry, axis=0)
         for a in range(4):
             new_entry = detectWallCollisionTime(i)
-            index = bisect.bisect(times_pw_float, float(new_entry[0,2])) #------ Problema con la salida formato array de detectWallCollisiontime
-            times_pw = np.insert(times_pw, index, new_entry)
+            index = bisect.bisect(times_pw_float, float(new_entry[0,2]))
+            times_pw = np.insert(times_pw, index, new_entry, axis=0)
     else:
+        j = int(j)
         # Particle-particle collision, recalculates entries involving i and j.
         times_pp = times_pp[~np.isin(times_pp, [i, j]).any(axis=1)]
         times_pw = times_pw[~np.isin(times_pw, [str(i), str(j)]).any(axis=1)]
     
-        times_pw[i] = detectWallCollisionTime(i)
-        times_pw[j] = detectWallCollisionTime(j)
+        for a in range(n_particles):
+            if a==i and a!=n_particles-1: # Avoid i-i case
+                a=a+1
+            new_entry = detectCollisionTime(i, a)
+            if new_entry[0,2] == 'inf':
+                index = -1
+            else:
+                index = bisect.bisect(times_pp[:,2], float(new_entry[0,2]))
+            times_pp = np.insert(times_pp, index, new_entry, axis=0)
+        for a in range(n_particles):
+            if (a==j or a==i) and a!=n_particles-1 : # Avoid j-j case and j-i case (already calculated)
+                a=a+1
+            new_entry = detectCollisionTime(j, a)
+            if new_entry[0,2] == 'inf':
+                index = -1
+            else:
+                index = bisect.bisect(times_pp[:,2], float(new_entry[0,2]))
+            times_pp = np.insert(times_pp, index, new_entry, axis=0)
+        
+        times_pw_float = np.array([float(a) for a in times_pw[:,2]])
+        for a in range(4):
+            new_entry = detectWallCollisionTime(i)
+            new_entry_j = detectWallCollisionTime(j)
+            index = bisect.bisect(times_pw_float, float(new_entry[0,2]))
+            index_j = bisect.bisect(times_pw_float, float(new_entry[0,2]))
+            times_pw = np.insert(times_pw, index, new_entry, axis=0)
+            times_pw = np.insert(times_pw, index_j, new_entry_j, axis=0)
+        
+
   
 def computeNextCollision():
     """ Propagates particles until next collision and updates velocities 
         after it. Checks if next col. is particle-particle or particle-wall """
+    global pos, vel
     t_pp = times_pp[0,2]
-    t_pw = times_pw[0,2]
+    t_pw = float(times_pw[0,2])
     # Check if particle-particle or particle-wall collision
     if t_pp <= t_pw:
         propagate(t_pp) # Propagate particles until current collision
-        i = times_pp[0,0]
-        j = times_pp[0,1]
+        i = int(times_pp[0,0])
+        j = int(times_pp[0,1])
         particleCollision(i, j) # Compute change in velocities due to collision
+        updateCollisionLists(t_pp, i, j)
     else:
         propagate(t_pw)
-        i = times_pw[0,0]
+        i = int(times_pw[0,0])
         wall = times_pw[0,1]
         wallCollision(i, wall)
+        updateCollisionLists(t_pw, i)
 
 def saveData(col_number):
     """ Saves the positions and velocities of every particle to an external
         file after current colision """
-    file_name_pos = data_folder + "/xy" + col_number + ".dat"
+    file_name_pos = data_folder + "/xy"+'{0:04d}'.format(col_number)+".dat"
     with open(file_name_pos,'w') as file:
         for i in range(n_particles):
             file.write('{0:10.2f} {1:10.2f}\n'.format(pos[i,0], pos[i,1]))
     file.closed
     
-    file_name_vel = data_folder + "/vxvy" + col_number + ".dat"
+    file_name_vel = data_folder + "/vxvy"+'{0:04d}'.format(col_number)+".dat"
     with open(file_name_vel,'w') as file:
         for i in range(n_particles):
             file.write('{0:10.2f} {1:10.2f}\n'.format(vel[i,0], vel[i,1]))
@@ -293,12 +337,13 @@ def saveData(col_number):
 initializeRandom()
 createWallCollisionList()
 createCollisionList()
-#for a in range(n_collisions):
- #   computeNextCollision()
- #   saveData(a)
+for a in range(n_collisions):
+    computeNextCollision()
+    saveData(a)
+    print("Saving file for colission nº: "+str(a))
     
-print(times_pw)
-print(times_pp)
+#print(times_pw)
+#print(times_pp)
         #bisect.bisect(np.array([float(a) for a in prueba[:,2]]))
         #bisect.insort(prueba, tau) # Bisect for creating an ordered list
 
