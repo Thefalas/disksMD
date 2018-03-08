4956f78be0b3436309263429297f6629d523de71
malopez <malopez00@gmail.com>
2018-03-08 19:06:10 +0100

    Some comments added about 2d histograms

--- src/statistics.py
+++ src/statistics.py
@@ -33,4 +33,6 @@ def velocityDistribution(n_collisions, data_folder):
     # With former array we can plot two histograms (for x and y directions)
     b = [-9,-8,-7,-6,-5,-4,-3,-2,-1,0,1,2,3,4,5,6,7,8,9]
     histPlot = plt.hist(vel, density=True, bins=b)
+    # Another interesting visualization
+#    histPlot2D = plt.hist2d(vel[:,0], vel[:,1], bins=b)
     return histPlot
